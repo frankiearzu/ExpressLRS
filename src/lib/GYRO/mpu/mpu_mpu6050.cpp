5e3e1b6091ee2466eba909710f4d0f3eefaa36d2
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2026-05-27 00:59:11 +0000

    feat(gyro): allow known MPU-6050 device IDs
    While the expected MPU-6050 device ID is 0x34, there are compatible
    components with other device IDs.

--- src/lib/GYRO/mpu/mpu_mpu6050.cpp
+++ src/lib/GYRO/mpu/mpu_mpu6050.cpp
@@ -23,7 +23,10 @@ static bool testConnection(MPU_Base* mpu) {
     bool ok = mpu->readRegister(MPU6050_RA_WHO_AM_I, &id, 1);
     id = (id >> 1) & 0x3F; // Bit 0 is reserved, starts at bit 1, 6 bits
     DBGLN("Id returned = 0x%x",id);
-    return ok && (id == 0x34); // 0x34 is the ID expected for this chip
+
+    // 0x34 is the ID expected for this chip, the other are known alernatives
+    bool known_mpu6050_id = (id == 0x34 || id == 0x38 || id == 0x39);
+    return ok && known_mpu6050_id;
 }
 
 bool MPUDev_MPU6050::initialize() {
