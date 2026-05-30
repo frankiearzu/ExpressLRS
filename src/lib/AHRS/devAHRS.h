14f66fb9a421eb4a5e4a1297e9b7b1e2da89ebf3
Frankie Arzu <32604366+frankiearzu@users.noreply.github.com>
2026-05-30 11:12:08 -0500

    Revert "Refactor to be able to use the G filter in both AHRS algorithms"
    This reverts commit c33bf85987db24be494ed0ad8d909f1675478b6d.

--- src/lib/AHRS/devAHRS.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "common.h"
-#include "device.h"
-
-extern device_t AHRS_device;
--- src/lib/Fusion/FusionAhrs.c
+++ src/lib/Fusion/FusionAhrs.c
@@ -15,8 +15,6 @@
 //------------------------------------------------------------------------------
 // Definitions
 
-#define HAS_MAGNETOMETER 0       // 1 to use the Magnetometer code
-
 /**
  * @brief Initial gain used during the initialisation.
  */
@@ -170,11 +168,9 @@ void FusionAhrsUpdate(FusionAhrs *const ahrs, const FusionVector gyroscope, cons
         }
     }
 
-
     // Calculate magnetometer feedback
     FusionVector halfMagnetometerFeedback = FUSION_VECTOR_ZERO;
     ahrs->magnetometerIgnored = true;
-#if HAS_MAGNETOMETER
     if (FusionVectorIsZero(magnetometer) == false) {
 
         // Calculate direction of magnetic field indicated by algorithm
@@ -205,17 +201,13 @@ void FusionAhrsUpdate(FusionAhrs *const ahrs, const FusionVector gyroscope, cons
             halfMagnetometerFeedback = ahrs->halfMagnetometerFeedback;
         }
     }
-#endif
 
     // Convert gyroscope to radians per second scaled by 0.5
     const FusionVector halfGyroscope = FusionVectorMultiplyScalar(gyroscope, FusionDegreesToRadians(0.5f));
 
     // Apply feedback to gyroscope
-#if HAS_MAGNETOMETER    
     const FusionVector adjustedHalfGyroscope = FusionVectorAdd(halfGyroscope, FusionVectorMultiplyScalar(FusionVectorAdd(halfAccelerometerFeedback, halfMagnetometerFeedback), ahrs->rampedGain));
-#else
-    const FusionVector adjustedHalfGyroscope = FusionVectorAdd(halfGyroscope, FusionVectorMultiplyScalar(halfAccelerometerFeedback, ahrs->rampedGain));
-#endif
+
     // Integrate rate of change of quaternion
     ahrs->quaternion = FusionQuaternionAdd(ahrs->quaternion, FusionQuaternionMultiplyVector(ahrs->quaternion, FusionVectorMultiplyScalar(adjustedHalfGyroscope, deltaTime)));
 
